fdcb5da1f8ebd7ba7242959abe75644989ff983a
ali faisal <alifaisalas99@gmail.com>
2020-03-10 18:59:46 +0500

    added-dict

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 from pre_proc import Preprocessing
 
 
+def removeWhitespaces(stop_list):
+    for sw in stop_list:
+        if sw.endswith(' '):
+            stop_list.remove(sw)
+            sw = sw.replace(' ', '')  # 'is ' => 'is'
+            stop_list.append(sw)
+    return stop_list
+
+
+def dictionary(stem_list):
+    book = {}
+    stem_list_len = len(stem_list)
+    for pos in range(0, stem_list_len):
+        stem = stem_list[pos]
+        if book.__contains__(stem):
+            li = book[stem]
+            li.append(pos)
+            book[stem] = li
+        else:
+            li = [pos]
+            book[stem] = li
+    return book
+
+
 if __name__ == '__main__':
-    file = open('Trump Speechs/speech_' + str(0) + ".txt", "r")
+    stop_file = open('Stopword-List.txt', "r")
+    # stop_list when imported from file has whitespaces, they should be removed
+    stop_list = removeWhitespaces(stop_file.read().split('\n'))
+    # print(stop_list)
+
+    doc_id = 0
+    file = open('Trump Speechs/speech_' + str(doc_id) + ".txt", "r")
     file.readline()  # after reading the line, moves file ptr to next line to skip the title
 
+    tokens = []
+    stems = []
+
     pipe = Preprocessing()
+    pipe.stop_word = stop_list
+
     tokens = pipe.tokenizer(file.read())
+    # print(tokens)     # use of stop-list reduces overall char size from 53K to 42K from file speech_0
     stems = pipe.stemmer(tokens)
     # print(stems)
-
-
+    dict_book = dictionary(stems)
+    print(dict_book)
--- pre_proc.py
+++ pre_proc.py
@@ -1,8 +1,11 @@
 from nltk.stem import PorterStemmer
+
+
 # only imported the Porter Stemmer
 
 # I created a class Preprocessing to encapsulate the token and stemming-wrapper methods
 class Preprocessing:
+    stop_word = []
 
     # a linear time function created which takes a file-buffer and performs the tokenizing
     def tokenizer(self, file_buffer):  # file_buffer is a string
@@ -19,35 +22,40 @@ class Preprocessing:
                 string += file_buffer[i]
                 c += 1
             elif ord(file_buffer[i]) in range(48, 58):  # ASCII[48-57] => [0-9]
-                while file_buffer[i] not in [" ", ":", "!", "?"]: # read the numbers until
+                while file_buffer[i] not in [" ", ":", "!", "?", "-"]:  # read the numbers until
                     # if decimal or comma occurs but after that there is no number
                     if file_buffer[i] in [".", ","] and ord(file_buffer[i + 1]) not in range(48, 58):  # '234,Now'
                         break
-                    string += file_buffer[i]    # append the numbers together
+                    string += file_buffer[i]  # append the numbers together
                     i += 1
-                tokens_L.append(string)
+                if string not in self.stop_word:
+                    tokens_L.append(string)
                 string = ""
                 c = 0  # reset the counter of chars when end of word
             else:
-                if file_buffer[i] in [" ", ":", ",", "!", "?"] and c != 0:  # indicates end of word
-                    tokens_L.append(string)
+                if file_buffer[i] in [" ", ":", ",", "!", "?", "-"] and c != 0:  # indicates end of word
+                    if string not in self.stop_word:
+                        tokens_L.append(string)
                     string = ""
                     c = 0  # reset the counter of chars when end of word
                 elif file_buffer[i] == ".":
                     # if no of chars before . are more than 1 then this is not an abbr but end of sentence/word
                     # e.g: Thousands.
                     if c > 1:
-                        tokens_L.append(string)
+                        if string not in self.stop_word:
+                            tokens_L.append(string)
                         string = ""
                     else:
-                        if file_buffer[i + 2] not in [" ", ".", ":", ",", "!", "?"]:  # not abbr, e.g:'U.S.A.Something '
-                            tokens_L.append(string)
+                        if file_buffer[i + 2] not in [" ", ".", ":", ",", "!", "?", "-"]:  # not abbr, e.g:'U.S.A.Something '
+                            if string not in self.stop_word:
+                                tokens_L.append(string)
                             string = ""
                     # else if no of chars before . is 1 then it is probably an abbr, so do not append in list now
                     # e.g: U.
                     c = 0  # reset the counter of chars before .
                 elif file_buffer[i] == "'":  # if char is apostrophe (`)
-                    tokens_L.append(string)  # add the string to list, if grammar is correct then will be c > 1
+                    if string not in self.stop_word:
+                        tokens_L.append(string)  # add the string to list, if grammar is correct then will be c > 1
                     string = ""
                     c = 0
                     i += 1  # move the ptr to next char possibly [s, re, ..]
@@ -55,18 +63,21 @@ class Preprocessing:
                         i += 1  # ignore everything after apostrophe until the list above
                 elif file_buffer[i] == "[":  # when this occurs, ignore in the inside of brackets, e.g: [applause]
                     if c != 0:  # this also marks the end of word, so if read char count > 1
-                        tokens_L.append(string)  # add the string to list
+                        if string not in self.stop_word:
+                            tokens_L.append(string)  # add the string to list
                         string = ""
                         c = 0
                     i += 1  # move the ptr inside the brackets [...
                     while file_buffer[i] not in ["]"]:  # read until ]
                         i += 1  # ignore everything inside
             i += 1
-        return tokens_L     # returns a prepared list of tokens
+        # tokens_L.remove('') - token list returned has empty strings in multiple indexes
+        # remove these before performing indexing
+        return tokens_L  # returns a prepared list of tokens
 
     def stemmer(self, tokensSP):
         stemsL = []
-        ps = PorterStemmer()       # imported from nltk library
-        for tk in tokensSP:     # pass token from token list in stem function and add stems in list
+        ps = PorterStemmer()  # imported from nltk library
+        for tk in tokensSP:  # pass token from token list in stem function and add stems in list
             stemsL.append(ps.stem(tk))
-        return stemsL   # returns a list of stems
+        return stemsL  # returns a list of stems
