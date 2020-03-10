7bef4aae0e8e5e10030aeb574b03ff25d64e02bf
ali faisal <alifaisalas99@gmail.com>
2020-03-10 16:28:29 +0500

    added-stemming

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from nltk.stem import PorterStemmer
 
 
 def tokenizer(file_buffer='U.S.A. has '):  # file_buffer is a string
-    tokens = []
+    tokens_L = []
     file_len = len(file_buffer)
     string = ""
     c = 0  # indicated no chars in a word before end of sentence, period (.), space ( ), colon (:)
     i = 0
-    while i < file_len:
+    while i < file_len:     # linear time operation
         if ord(file_buffer[i]) in range(65, 91):  # ASCII[65-90] => [A-Z]
             string += chr(ord(file_buffer[i]) + 32)  # A => a, .., Z => z
             c += 1  # inc the no chars read
@@ -16,33 +16,33 @@ def tokenizer(file_buffer='U.S.A. has '):  # file_buffer is a string
             c += 1
         elif ord(file_buffer[i]) in range(48, 58):  # ASCII[48-57] => [0-9]
             while file_buffer[i] not in [" ", ":", "!", "?"]:
-                if file_buffer[i] in [".", ","] and ord(file_buffer[i+1]) not in range(48, 58):  # '234,Now'
+                if file_buffer[i] in [".", ","] and ord(file_buffer[i + 1]) not in range(48, 58):  # '234,Now'
                     break
                 string += file_buffer[i]
                 i += 1
-            tokens.append(string)
+            tokens_L.append(string)
             string = ""
             c = 0  # reset the counter of chars when end of word
         else:
             if file_buffer[i] in [" ", ":", ",", "!", "?"] and c != 0:  # indicates end of word
-                tokens.append(string)
+                tokens_L.append(string)
                 string = ""
                 c = 0  # reset the counter of chars when end of word
             elif file_buffer[i] == ".":
                 # if no of chars before . are more than 1 then this is not an abbr but end of sentence/word
                 # e.g: Thousands.
                 if c > 1:
-                    tokens.append(string)
+                    tokens_L.append(string)
                     string = ""
                 else:
                     if file_buffer[i + 2] not in [" ", ".", ":", ",", "!", "?"]:  # not abbr, e.g:'U.S.A.Something '
-                        tokens.append(string)
+                        tokens_L.append(string)
                         string = ""
                 # else if no of chars before . is 1 then it is probably an abbr, so do not append in list now
                 # e.g: U.
                 c = 0  # reset the counter of chars before .
             elif file_buffer[i] == "'":  # if char is apostrophe (`)
-                tokens.append(string)  # add the string to list
+                tokens_L.append(string)  # add the string to list
                 string = ""
                 c = 0
                 i += 1
@@ -50,15 +50,22 @@ def tokenizer(file_buffer='U.S.A. has '):  # file_buffer is a string
                     i += 1
             elif file_buffer[i] == "[":
                 if c != 0:
-                    tokens.append(string)  # add the string to list
+                    tokens_L.append(string)  # add the string to list
                     string = ""
                     c = 0
                 i += 1
                 while file_buffer[i] not in ["]"]:
                     i += 1
-
         i += 1
-    return tokens
+    return tokens_L
+
+
+def stemmer(tokensSP):
+    stemsL = []
+    ps = PorterStemmer()
+    for tk in tokensSP:
+        stemsL.append(ps.stem(tk))
+    return stemsL
 
 
 if __name__ == '__main__':
@@ -66,6 +73,7 @@ if __name__ == '__main__':
     file.readline()  # after reading the line, moves file ptr to next line to skip the title
 
     # tokensM = tokenizer()
-    tokensM = tokenizer(file.read())
+    tokens = tokenizer(file.read())
+    stems = stemmer(tokens)
 
-    print(tokensM)
+    print(stems)
